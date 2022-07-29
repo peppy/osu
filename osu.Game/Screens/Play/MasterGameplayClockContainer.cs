bf5e191164ef344c2776a8e60436c92185803367
Dean Herbert <pe@ppy.sh>
2022-07-29 18:52:31 +0900

    Move out `OffsetCorrectionClock`

--- osu.Game/Screens/Play/MasterGameplayClockContainer.cs
+++ osu.Game/Screens/Play/MasterGameplayClockContainer.cs
@@ -268,49 +268,4 @@ namespace osu.Game.Screens.Play
             }
         }
     }
-
-    public class OffsetCorrectionClock : FramedOffsetClock
-    {
-        private readonly BindableDouble pauseRateAdjust;
-
-        private double offset;
-
-        public new double Offset
-        {
-            get => offset;
-            set
-            {
-                if (value == offset)
-                    return;
-
-                offset = value;
-
-                updateOffset();
-            }
-        }
-
-        public double RateAdjustedOffset => base.Offset;
-
-        public OffsetCorrectionClock(IClock source, BindableDouble pauseRateAdjust)
-            : base(source)
-        {
-            this.pauseRateAdjust = pauseRateAdjust;
-        }
-
-        public override void ProcessFrame()
-        {
-            base.ProcessFrame();
-            updateOffset();
-        }
-
-        private void updateOffset()
-        {
-            // changing this during the pause transform effect will cause a potentially large offset to be suddenly applied as we approach zero rate.
-            if (pauseRateAdjust.Value == 1)
-            {
-                // we always want to apply the same real-time offset, so it should be adjusted by the difference in playback rate (from realtime) to achieve this.
-                base.Offset = Offset * Rate;
-            }
-        }
-    }
 }
