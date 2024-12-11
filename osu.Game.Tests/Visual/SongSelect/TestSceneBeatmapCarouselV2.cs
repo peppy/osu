a2ee5fc0ac02fc9b38dfc37663be6d67bbfe8d5a
Dean Herbert <pe@ppy.sh>
2024-12-11 15:38:00 +0900

    Setup very basic visuals

--- osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapCarouselV2.cs
+++ osu.Game.Tests/Visual/SongSelect/TestSceneBeatmapCarouselV2.cs
@@ -2,18 +2,54 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Testing;
-using osu.Game.Screens.Select;
+using osu.Game.Beatmaps;
+using osu.Game.Database;
+using osu.Game.Screens.SelectV2;
+using osu.Game.Tests.Beatmaps;
+using osu.Game.Tests.Resources;
+using osuTK;
 
 namespace osu.Game.Tests.Visual.SongSelect
 {
     [TestFixture]
     public partial class TestSceneBeatmapCarouselV2 : OsuManualInputManagerTestScene
     {
+        private readonly BindableList<BeatmapSetInfo> beatmaps = new BindableList<BeatmapSetInfo>();
+
+        [Cached(typeof(BeatmapStore))]
+        private BeatmapStore store;
+
+        public TestSceneBeatmapCarouselV2()
+        {
+            store = new TestBeatmapStore
+            {
+                BeatmapSets = { BindTarget = beatmaps }
+            };
+        }
+
         [SetUpSteps]
         public void SetUpSteps()
         {
-            AddStep("create carousel", () => Child = new BeatmapCarouselV2());
+            AddStep("create components", () =>
+            {
+                beatmaps.Clear();
+
+                Child = new BeatmapCarouselV2
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new Vector2(500, 700),
+                };
+            });
+            AddStep("add beatmaps", () =>
+            {
+                for (int i = 0; i < 10; i++)
+                    beatmaps.Add(TestResources.CreateTestBeatmapSetInfo());
+            });
         }
 
         [Test]
