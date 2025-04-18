601560b3c5932599bba1912eeb8d1164d0533769
Dean Herbert <pe@ppy.sh>
2025-04-18 19:22:09 +0900

    Change equality to allow non-reference comparisons
    This is required to hold selection when beatmaps are updates, as one
    important case.

--- osu.Game/Graphics/Carousel/Carousel.cs
+++ osu.Game/Graphics/Carousel/Carousel.cs
@@ -167,6 +167,11 @@ namespace osu.Game.Graphics.Carousel
         protected virtual Task FilterAsync() => filterTask = performFilter();
 
         /// <summary>
+        /// Check whether two models are the same for display purposes.
+        /// </summary>
+        protected virtual bool CheckModelEquality(object x, object y) => ReferenceEquals(x, y);
+
+        /// <summary>
         /// Create a drawable for the given carousel item so it can be displayed.
         /// </summary>
         /// <remarks>
@@ -490,10 +495,10 @@ namespace osu.Game.Graphics.Carousel
 
                 updateItemYPosition(item, ref lastVisible, ref yPos);
 
-                if (ReferenceEquals(item.Model, currentKeyboardSelection.Model))
+                if (CheckModelEquality(item.Model, currentKeyboardSelection.Model!))
                     currentKeyboardSelection = new Selection(item.Model, item, item.CarouselYPosition, i);
 
-                if (ReferenceEquals(item.Model, currentSelection.Model))
+                if (CheckModelEquality(item.Model, currentSelection.Model!))
                     currentSelection = new Selection(item.Model, item, item.CarouselYPosition, i);
             }
 
@@ -578,7 +583,7 @@ namespace osu.Game.Graphics.Carousel
 
                 panel.X = GetPanelXOffset(panel);
 
-                c.Selected.Value = c.Item == currentSelection?.CarouselItem;
+                c.Selected.Value = currentSelection?.CarouselItem != null && CheckModelEquality(c.Item, currentSelection.CarouselItem);
                 c.KeyboardSelected.Value = c.Item == currentKeyboardSelection?.CarouselItem;
                 c.Expanded.Value = c.Item.IsExpanded;
             }
@@ -644,7 +649,7 @@ namespace osu.Game.Graphics.Carousel
 
                 // The case where we're intending to display this panel, but it's already displayed.
                 // Note that we **must compare the model here** as the CarouselItems may be fresh instances due to a filter operation.
-                var existing = toDisplay.FirstOrDefault(i => i.Model == carouselPanel.Item!.Model);
+                var existing = toDisplay.FirstOrDefault(i => CheckModelEquality(i.Model, carouselPanel.Item!.Model));
 
                 if (existing != null)
                 {
