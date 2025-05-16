109e71d171ba10bf75512354f3dc3348c956e58c
Dean Herbert <pe@ppy.sh>
2025-05-16 19:00:25 +0900

    Tidy up `SoloSongSelect` class

--- osu.Game/Screens/SelectV2/SoloSongSelect.cs
+++ osu.Game/Screens/SelectV2/SoloSongSelect.cs
@@ -18,10 +18,6 @@ namespace osu.Game.Screens.SelectV2
     public partial class SoloSongSelect : SongSelect
     {
         private PlayerLoader? playerLoader;
-        private IReadOnlyList<Mod>? modsAtGameplayStart;
-
-        [Resolved]
-        private INotificationOverlay? notifications { get; set; }
 
         public override bool EditingAllowed => true;
 
@@ -29,56 +25,18 @@ namespace osu.Game.Screens.SelectV2
         {
             if (playerLoader != null) return false;
 
-            modsAtGameplayStart = Mods.Value;
-
-            // Ctrl+Enter should start map with autoplay enabled.
-            if (GetContainingInputManager()?.CurrentState?.Keyboard.ControlPressed == true)
-            {
-                var autoInstance = getAutoplayMod();
-
-                if (autoInstance == null)
-                {
-                    notifications?.Post(new SimpleNotification
-                    {
-                        Text = NotificationsStrings.NoAutoplayMod
-                    });
-                    return false;
-                }
-
-                var mods = Mods.Value.Append(autoInstance).ToArray();
-
-                if (!ModUtils.CheckCompatibleSet(mods, out var invalid))
-                    mods = mods.Except(invalid).Append(autoInstance).ToArray();
-
-                Mods.Value = mods;
-            }
+            addAutoplayIfControlPressed();
 
             this.Push(playerLoader = new PlayerLoader(createPlayer));
             return true;
-
-            Player createPlayer()
-            {
-                Player player;
-
-                var replayGeneratingMod = Mods.Value.OfType<ICreateReplayData>().FirstOrDefault();
-
-                if (replayGeneratingMod != null)
-                {
-                    player = new ReplayPlayer((beatmap, mods) => replayGeneratingMod.CreateScoreFromReplayData(beatmap, mods));
-                }
-                else
-                {
-                    player = new SoloPlayer();
-                }
-
-                return player;
-            }
         }
 
         public override void OnResuming(ScreenTransitionEvent e)
         {
             base.OnResuming(e);
-            revertMods();
+
+            revertAutoplayAfterPlay();
+            playerLoader = null;
         }
 
         public override bool OnExiting(ScreenExitEvent e)
@@ -86,19 +44,60 @@ namespace osu.Game.Screens.SelectV2
             if (base.OnExiting(e))
                 return true;
 
-            revertMods();
+            revertAutoplayAfterPlay();
+            playerLoader = null;
             return false;
         }
 
-        private ModAutoplay? getAutoplayMod() => Ruleset.Value.CreateInstance().GetAutoplayMod();
+        private Player createPlayer()
+        {
+            var replayGeneratingMod = Mods.Value.OfType<ICreateReplayData>().FirstOrDefault();
 
-        private void revertMods()
+            if (replayGeneratingMod != null)
+                return new ReplayPlayer((beatmap, mods) => replayGeneratingMod.CreateScoreFromReplayData(beatmap, mods));
+
+            return new SoloPlayer();
+        }
+
+        #region Handling autoplay via ctrl key
+
+        private IReadOnlyList<Mod>? modsAtGameplayStart;
+
+        [Resolved]
+        private INotificationOverlay? notifications { get; set; }
+
+        private void addAutoplayIfControlPressed()
+        {
+            modsAtGameplayStart = Mods.Value;
+
+            // Ctrl+Enter should start map with autoplay enabled.
+            if (GetContainingInputManager()?.CurrentState?.Keyboard.ControlPressed == true)
+            {
+                var autoInstance = Ruleset.Value.CreateInstance().GetAutoplayMod();
+
+                if (autoInstance == null)
+                {
+                    notifications?.Post(new SimpleNotification { Text = NotificationsStrings.NoAutoplayMod });
+                    return;
+                }
+
+                Mod[] mods = Mods.Value.Append(autoInstance).ToArray();
+
+                if (!ModUtils.CheckCompatibleSet(mods, out var invalid))
+                    mods = mods.Except(invalid).Append(autoInstance).ToArray();
+
+                Mods.Value = mods;
+            }
+        }
+
+        private void revertAutoplayAfterPlay()
         {
             if (playerLoader == null) return;
 
             Mods.Value = modsAtGameplayStart;
-            playerLoader = null;
         }
+
+        #endregion
 
         private partial class PlayerLoader : Play.PlayerLoader
         {
