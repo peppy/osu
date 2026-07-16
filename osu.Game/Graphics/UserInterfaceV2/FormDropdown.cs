fec525a51795306e87672430eb256900d28829b6
Dean Herbert <pe@ppy.sh>
2026-07-16 21:59:50 +0900

    Add filter to hide full rooms in multiplayer lobby (#38318)
    This also upgrades the components to use newer designs.
    
    Special care taken to make this flow when UI scale is higher. It might
    not look pretty (there's still some minor overlap) but it's better than
    it used to be.
    
    Of note, this is filtered locally. I'm still doing a server request
    because it's simpler than creating a separate flow for this. Can
    probably be improved in the future.
    
    - Addresses https://github.com/ppy/osu/discussions/38294.
    - [x] Depends on https://github.com/ppy/osu/pull/38317 to look correct.
    
    
    https://github.com/user-attachments/assets/e015454b-8667-4ec8-8b30-a63220034e6b
    
    ---------
    
    Co-authored-by: Bartłomiej Dach <dach.bartlomiej@gmail.com>

--- osu.Game/Graphics/UserInterfaceV2/FormDropdown.cs
+++ osu.Game/Graphics/UserInterfaceV2/FormDropdown.cs
@@ -164,7 +164,8 @@ namespace osu.Game.Graphics.UserInterfaceV2
                     {
                         RelativeSizeAxes = Axes.X,
                         AutoSizeAxes = Axes.Y,
-                        Padding = new MarginPadding(9),
+                        // Adjustments made to match height of `FormCheckbox` in most compact format (when no labels are present).
+                        Padding = new MarginPadding { Horizontal = 9, Top = 5, Bottom = 9 },
                         Children = new Drawable[]
                         {
                             new FillFlowContainer
