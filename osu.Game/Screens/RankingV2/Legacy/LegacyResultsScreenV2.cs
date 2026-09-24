2b26c94b9efa7903e8b861a9eeeffa85936fc8ab
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-09-24 10:18:26 +0200

    delete unnecessary component
    the more generic one works here fine, there'll be no transforms on this
    anyhow

--- osu.Game/Screens/RankingV2/Legacy/LegacyResultsScreenV2.cs
+++ osu.Game/Screens/RankingV2/Legacy/LegacyResultsScreenV2.cs
@@ -3,8 +3,11 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Game.Scoring;
 using osu.Game.Screens.Play;
+using osu.Game.Skinning;
+using osuTK;
 
 namespace osu.Game.Screens.RankingV2.Legacy
 {
@@ -33,7 +36,14 @@ namespace osu.Game.Screens.RankingV2.Legacy
             [
                 new LegacyRankingBackgroundOverlay(),
                 new LegacyRankingDetails(),
-                new LegacyRankingTitle(),
+                new SkinnableSprite
+                {
+                    SpriteName = { Value = @"ranking-title" },
+                    // TODO: move to skinnable container defaults
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Position = new Vector2(-20, 0) * LegacySkin.STABLE_MAGIC_SCALE_FACTOR,
+                },
                 new LegacyRankingPanel(),
                 new LegacyRankingGraph(),
             ];
